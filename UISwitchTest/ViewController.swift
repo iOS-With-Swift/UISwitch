09a19958386917b52f7f77a5e4fb2865a780cbad
Lê Hồng Phong <lehongphong@Les-Mac.local>
2017-03-20 07:02:57 -0700

    first commit

--- UISwitchTest/ViewController.swift
+++ UISwitchTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchbtn: UISwitch!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func switchController(_ sender: Any) {
+        if switchbtn.isOn {
+            label.textColor = UIColor.black
+            view.backgroundColor = UIColor.white
+        }else{
+            label.textColor=UIColor.white
+            view.backgroundColor=UIColor.gray
+        }
+    }
 
 }
 
