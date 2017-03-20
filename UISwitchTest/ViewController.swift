c497c62b130ffd18501a7f085e216c2df09ef1f6
Lê Hồng Phong <lehongphong@Les-Mac.local>
2017-03-20 06:37:39 -0700

    Initial Commit

---
+++ UISwitchTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UISwitchTest
+//
+//  Created by Lê Hồng Phong on 3/20/17.
+//  Copyright © 2017 Lê Hồng Phong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
